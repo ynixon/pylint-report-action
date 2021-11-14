bac83aa2e25443845c08f4500910dac86c37dfd8
Yossi Nixon <ynixon@gmail.com>
2021-11-14 02:02:56 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,11 +14,11 @@ echo "Starting ${GITHUB_WORKFLOW}:${GITHUB_ACTION}"
 
 mkdir $(dirname "$3")
 export PYTHONPATH=$(which python)
-echo -e "pylint \"$1\" \"$2\" \| pylint_report.py \> \"$3\""
+echo -e "pylint $1 $2 | pylint_report.py > $3"
 pwd
 ls -la
 ls -la /
-pylint "$1" "$2"| pylint_report.py > "$3"
+pylint $1 $2| pylint_report.py > $3
 
 echo "#################################################"
 echo "Completed ${GITHUB_WORKFLOW}:${GITHUB_ACTION}"
